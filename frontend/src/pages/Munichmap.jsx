f471dd6585ca1559c58c7903cc1ccca27974432d
JohannesGetzner <j.getzner@gmail.com>
2023-11-18 17:13:24 +0100

    changed nav icons, adjusted layout, added searchBar, and basic map

--- frontend/src/pages/Munichmap.jsx
+++ frontend/src/pages/Munichmap.jsx
@@ -1,10 +1,44 @@
-import React from 'react'
-import VisualCueCard from '../components/visualCueCard/VisualCueCard'
+import React, { useRef, useEffect, useState } from 'react';
+import maplibregl from 'maplibre-gl';
+import 'maplibre-gl/dist/maplibre-gl.css';
+import SearchBar from "../components/searchbar/SearchBar";
 
 const Munichmap = () => {
-  return (
-    <VisualCueCard></VisualCueCard>
-  )
-}
+  const mapboxToken = 'YOUR_MAPBOX_TOKEN'; // Replace with your Mapbox token
 
-export default Munichmap
+  const initialViewport = {
+    latitude: 48.8566, // Munich's latitude
+    longitude: 2.3522, // Munich's longitude
+    zoom: 10,
+  };
+
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng] = useState(11.576124);
+  const [lat] = useState(48.137154);
+  const [zoom] = useState(14);
+  const [API_KEY] = useState('d8Ep6nL1m5luLcZ2oC3u');
+
+  useEffect(() => {
+    if (map.current) return;
+
+    map.current = new maplibregl.Map({
+      container: mapContainer.current,
+      style: `https://api.maptiler.com/maps/streets-v2/style.json?key=${API_KEY}`,
+      center: [lng, lat],
+      zoom: zoom,
+    });
+  }, [API_KEY, lng, lat, zoom]);
+
+  return (
+      <div className="map-wrap" style={{ width: '100%', height: '100%' }}>
+        {/* Map container */}
+        <div ref={mapContainer} className="map" style={{ width: '100vh', height: '100%' }} />
+
+        {/* SearchBar component */}
+        <SearchBar />
+      </div>
+  );
+};
+
+export default Munichmap;
